36af9f7b42b53780cffb8e4cfc03f8337cfbf479
Fabienne Marquant <fabienne.marquant@protonmail.com>
2021-04-11 21:33:14 +0200

    jeu commit 9

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('_1/', views.post_list_1, name='post_list_1'),
     path('', views.post_list, name='post_list'),
+    path('post_list_1/', views.post_list_1, name='post_list_1'),
 ]
